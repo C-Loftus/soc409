8e54f62a077cf9db7537c0f5a1e977c8c01d4808
colton <cloftus@princeton.edu>
2022-03-14 22:58:08 -0400

    its working :DD

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ import os
 from flask import Flask
 import flask
 
-template_dir = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__, template_folder=template_dir)
+# template_dir = os.path.dirname(os.path.abspath(__file__))
+# app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
