ccaf55f91a41c40350e4f870d53c91a378864707
colton <cloftus@princeton.edu>
2022-03-23 14:16:46 -0400

    working backend for photo uploads

--- app/main.py
+++ app/main.py
@@ -5,15 +5,21 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
-from . import utils
+from .utils import files
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config["IMAGE_UPLOADS"] = ROOT + "/images"
+app.config["IMAGE_UPLOADS"] = ROOT + "/static/assets/images"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
 app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,9 +38,8 @@ def commitPost(request, image_link=""):
     plant_Name =      request.form.get('plantName', False)
     post_title =      request.form.get('title', False)
     post_content =    request.form.get('description', False)
-    post_location =   request.form.get('location', False)
     post_author = request.form.get('author', False)
-    new_post = Post(title=post_title, description=post_content, location=post_location, plantName=plant_Name, imageLink=image_link, author=post_author)
+    new_post = Post(title=post_title, description=post_content, plantName=plant_Name, imageLink=image_link, author=post_author)
     db.session.add(new_post)
     db.session.commit()
 
@@ -45,17 +50,16 @@ def newPost():
             image = request.files["image"]
             
             if "filesize" in request.cookies:
-                if not utils.allowed_image_filesize(app, request.cookies["filesize"]):
+                if not files.allowed_image_filesize(app, request.cookies["filesize"]):
                     print("Filesize exceeded maximum limit")
                     return redirect(request.url)
 
-            if utils.allowed_image(image.filename):
-                filename = secure_filename(image.filename)
-                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
-                image_link = filename
+            if files.allowed_image(app, image.filename):
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+                image_link = image.filename
 
         commitPost(request, image_link)
-        return redirect('/')
+        return redirect('/allposts')
     elif request.method == 'GET':
         all_posts = Post.query.order_by(Post.date_posted).all()
         return render_template('newpost.html', posts=all_posts)
@@ -67,5 +71,5 @@ def allPosts():
     return render_template('allPosts.html', posts=all_posts)
 
 @app.route("/")
-def hello_world():
+def index():
     return flask.render_template('index.html')
